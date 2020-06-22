79ade0d285566c2311f9db20c9d1edfae273baff
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-06-22 17:49:55 +0200

    Initial commit
    ADD: Sandbox terrain
    ADD: Sandbox sample objects (huts, trees etc)
    ADD: Player nav mesh agent, mover script, target to reach

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+	//Config parameters
+	[SerializeField] GameObject target;
+
+	//Cache
+	NavMeshAgent nma;
+	void Start()
+	{
+		nma = GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+	{
+		MoveToTarget();
+	}
+
+	private void MoveToTarget()
+	{
+		if (!nma) { return; }
+
+		else
+		{
+			nma.destination = target.transform.position;
+		}
+	}
+}
