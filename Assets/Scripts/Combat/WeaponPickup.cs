33f2b9a397107b8ca66fb14fe50db0a63767b583
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-07-28 14:49:41 +0200

    ADD: Weapon pickups respawn after a while after picking up

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -8,13 +8,33 @@ namespace RPGCourse.Combat
 	{
 		//Config parameters
 		[SerializeField] Weapon weaponType = null;
+		[SerializeField] float respawnTime = 5f;
 
 		private void OnTriggerEnter(Collider other)
 		{
 			if (other.tag == "Player")
 			{
 				other.GetComponent<Fighter>().EquipWeapon(weaponType);
-				Destroy(gameObject);
+				StartCoroutine(HideForSeconds(respawnTime));
+			}
+		}
+
+		IEnumerator HideForSeconds(float time)
+		{
+			ShowPickup(false);			
+
+			yield return new WaitForSeconds(time);
+
+			ShowPickup(true);
+		}
+
+		private void ShowPickup(bool value)
+		{
+			GetComponent<CapsuleCollider>().enabled = value;
+
+			foreach(Transform child in transform)
+			{
+				child.gameObject.SetActive(value);
 			}
 		}
 	}
