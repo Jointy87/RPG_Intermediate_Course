4dcd20c637377834bca7eacab91420d592ba2206
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-08-06 19:20:06 +0200

    ADD: now raycast does navmesh sample to check if it can go to certain areas of navmesh ADD: calculates navmesh path and can't click if click is too far

--- Assets/Scripts/Control/IRaycastable.cs
+++ Assets/Scripts/Control/IRaycastable.cs
@@ -3,6 +3,7 @@
 	public interface IRaycastable
 	{
 		bool HandleRaycast(PlayerController callingController);
+		
 		CursorType GetCursorType();
 	}
 }
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -6,6 +6,7 @@ using RPGCourse.Combat;
 using RPGCourse.Resources;
 using System;
 using UnityEngine.EventSystems;
+using UnityEngine.AI;
 
 namespace RPGCourse.Control
 {
@@ -13,6 +14,7 @@ namespace RPGCourse.Control
 	{	
 		//Config parameters
 		[SerializeField] CursorMapping[] cursorMappings = null;
+		[SerializeField] float maxPathDistance = 25f;
 
 		//Cache
 		Health health;
@@ -94,19 +96,56 @@ namespace RPGCourse.Control
 
 		private bool MovementInteraction()
 		{
-			RaycastHit hit;
-			bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+			Vector3 target;
+			bool hasHit = RayCastNavMesh(out target);
 
 			if(hasHit)
 			{
 				if (Input.GetMouseButton(0))
 				{
-					GetComponent<Mover>().StartMoveAction(hit.point, 1f);
+					GetComponent<Mover>().StartMoveAction(target, 1f);
 				} 
 				SetCursor(CursorType.Movement);
 				return true;
 			}
 			return false;
+		}
+
+		private bool RayCastNavMesh(out Vector3 target)
+		{
+			target = new Vector3();
+
+			RaycastHit hit;
+			bool hasRayHit = Physics.Raycast(GetMouseRay(), out hit);
+			if(!hasRayHit) return false;
+
+			NavMeshHit navMeshHit;
+			if(!NavMesh.SamplePosition
+				(hit.point, out navMeshHit, .3f, NavMesh.AllAreas)) return false;
+
+			target = navMeshHit.position;
+
+			NavMeshPath path = new NavMeshPath();
+			bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+			if(!hasPath || path.status != NavMeshPathStatus.PathComplete) return false;
+
+			if(GetPathLength(path) > maxPathDistance) return false;
+			
+			return true;
+		}
+
+		private float GetPathLength(NavMeshPath path)
+		{
+			float totalDistance = 0;
+
+			if(path.corners.Length < 2) return totalDistance;
+
+			for (int corner = 0; corner < path.corners.Length - 1; corner++)
+			{
+				totalDistance = Vector3.Distance(path.corners[corner], path.corners[corner + 1]);
+			}
+			print(totalDistance);
+			return totalDistance;
 		}
 
 		private static Ray GetMouseRay()
